5a080b2fe522ea39c5a5985bdfd32627b65e9192
yuanliuvsco <yuan@vsco.co>
2016-09-23 17:03:34 -0700

    Added a toggle to use different color spaces as working space

--- CIRAWFilterColorShiftBugTest/ViewController.m
+++ CIRAWFilterColorShiftBugTest/ViewController.m
@@ -11,44 +11,60 @@
 @import CoreImage;
 @import AssetsLibrary;
 
-static NSString *kImageFileURLPath = @"TestGradient";
-static NSString *kImageFileURLExtension = @"png";
+static NSString *kImageFileURLPath = @"IMG_1453";
+static NSString *kImageFileURLExtension = @"CR2";
 
 @interface ViewController ()
 
 @property (strong, nonatomic) UIImage *inputUIImage;
 @property (strong, nonatomic) UIImage *outputUIImage;
-
+@property (weak, nonatomic) IBOutlet UISwitch *colorspaceSwitch;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
 
+- (void)LoadImageUsingWorkingColorSpace: (CGColorSpaceRef) workingColorSpace
+{
+    
     NSBundle *bundle = [NSBundle mainBundle];
     NSURL *imageURL = [NSURL fileURLWithPath:[bundle pathForResource:kImageFileURLPath ofType:kImageFileURLExtension]];
     assert(imageURL);
     
     CIFilter *CIRAWFilter = [CIFilter filterWithImageURL:imageURL options:@{}];
-
+    [CIRAWFilter setValue:@(0.3457) forKey:kCIInputNeutralChromaticityXKey];
+    [CIRAWFilter setValue:@(0.3585) forKey:kCIInputNeutralChromaticityYKey];
+    
     CIImage *CIRAWFilterOutputImage = CIRAWFilter.outputImage;
     
     CIContext *context = [CIContext contextWithOptions:@{kCIContextWorkingFormat:@(kCIFormatRGBAh),
-                                                         kCIContextUseSoftwareRenderer:@YES
+                                                         kCIContextUseSoftwareRenderer:@YES,
+                                                         kCIContextWorkingColorSpace: (__bridge id)workingColorSpace,
+                                                         kCIContextOutputColorSpace: (__bridge id)CGColorSpaceCreateWithName(kCGColorSpaceDisplayP3)
                                                          }];
     
     CGImageRef CIRAWFilterOutputCGImage = [context createCGImage:CIRAWFilterOutputImage fromRect:CIRAWFilterOutputImage.extent];
- 
-    self.inputUIImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@.%@", kImageFileURLPath, kImageFileURLExtension]];
     self.outputUIImage = [UIImage imageWithCGImage:CIRAWFilterOutputCGImage];
-    
+    CGImageRelease(CIRAWFilterOutputCGImage);
     [self.imageView setImage:self.outputUIImage];
 
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self LoadImageUsingWorkingColorSpace: CGColorSpaceCreateWithName(kCGColorSpaceGenericRGBLinear)];
+
+}
+
+- (IBAction)colorspaceSwitchToggle:(UISwitch *)sender {
+    if ([sender isOn]){
+        [self LoadImageUsingWorkingColorSpace: CGColorSpaceCreateWithName(kCGColorSpaceDisplayP3)];
+    }else{
+        [self LoadImageUsingWorkingColorSpace: CGColorSpaceCreateWithName(kCGColorSpaceGenericRGBLinear)];
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -74,6 +90,7 @@ static NSString *kImageFileURLExtension = @"png";
     showInput = !showInput;
     
     if (showInput) {
+        self.inputUIImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@.%@", kImageFileURLPath, kImageFileURLExtension]];
         [self.imageView setImage:self.inputUIImage];
     } else {
         [self.imageView setImage:self.outputUIImage];
